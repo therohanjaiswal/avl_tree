dcb4d5966cc854ef001b5d37a8c74c2f75930042
Rohan <gateway2rohan@gmail.com>
2021-09-05 01:09:56 +0530

    Perform Deletion

--- avl_tree.cpp
+++ avl_tree.cpp
@@ -26,12 +26,10 @@ public:
     friend class AVL_Tree;
 };
 
-int result = 0;
 class AVL_Tree
 {
 private:
     AVL_Node *head;
-    AVL_Node *parentOfLeftMostNode(AVL_Node *);
 
 public:
     AVL_Tree()
@@ -41,7 +39,7 @@ public:
     void AVL_Insert(int);
     void AVL_Delete(int);
     bool AVL_Search(int);
-    void AVL_Print();
+    void AVL_Print(const char *);
     ~AVL_Tree()
     {
         delete head;
@@ -195,92 +193,175 @@ void AVL_Tree::AVL_Insert(int k)
 
 void AVL_Tree::AVL_Delete(int k)
 {
+    stack<AVL_Node *> stack;
+    stack.push(head);
     try
     {
-        if (AVL_Search(k))
+        AVL_Node *root = head->RChild;
+        AVL_Node *curr = root, *par = head;
+        while (curr != nullptr && curr->key != k)
         {
-            AVL_Node *root = head->RChild;
-            AVL_Node *temp = root, *par = head;
-            while (temp->key != k)
-            {
-                par = temp;
-                if (temp->key < k)
-                    temp = temp->RChild;
-                else if (temp->key > k)
-                    temp = temp->LChild;
-            }
-
-            // leaf node deletion
-            if (temp->LChild == nullptr && temp->RChild == nullptr)
-            {
-                if (temp->key < par->key)
-                    par->LChild = nullptr;
-                else
-                    par->RChild = nullptr;
-            }
-
-            // if temp has single right child
-            if (temp->LChild == nullptr && temp->RChild != nullptr)
-            {
-                temp->key = temp->RChild->key;
-                // free right child node later
-                temp->RChild = nullptr;
-            }
-
-            // if temp has single left child
-            if (temp->LChild != nullptr && temp->RChild == nullptr)
-            {
-                temp->key = temp->LChild->key;
-                // free left child node later
-                temp->LChild = nullptr;
-            }
-
-            if (temp->LChild != nullptr && temp->RChild != nullptr)
-            {
-                AVL_Node *leftMostPar = parentOfLeftMostNode(temp->RChild);
-                if (leftMostPar == nullptr)
-                {
-                    temp->key = temp->RChild->key;
-                    temp->RChild = nullptr;
-                }
-                else
-                {
-                    AVL_Node *leftMost = leftMostPar->LChild;
-                    // if lefmost node has no child
-                    if (leftMost->RChild == nullptr)
-                    {
-                        temp->key = leftMost->key;
-                        leftMostPar->LChild = nullptr;
-                    }
-                    else
-                    { // if leftmost node has right child
-                        temp->key = leftMost->key;
-                        leftMost->key = leftMost->RChild->key;
-                        // free right child later
-                    }
-                }
-            }
+            par = curr;
+            stack.push(par);
+            if (curr->key < k)
+                curr = curr->RChild;
+            else if (curr->key > k)
+                curr = curr->LChild;
         }
+
+        // k doesn't exists
+        if (curr == nullptr)
+            throw k;
+
+        // leaf node deletion
+        if (curr->LChild == nullptr && curr->RChild == nullptr)
+        {
+            if (par == head)
+                head->RChild = curr->RChild;
+            else if (curr->key < par->key)
+                par->LChild = nullptr;
+            else
+                par->RChild = nullptr;
+            // free(curr);
+        }
+        // node with single child deletion
+        else if (curr->LChild == nullptr || curr->RChild == nullptr)
+        {
+            AVL_Node *temp;
+            if (curr->LChild == nullptr)
+                temp = curr->RChild;
+            else
+                temp = curr->LChild;
+
+            // if deleted node is root node itself with one child
+            if (par == head)
+                head->RChild = temp;
+            else if (curr->key > par->key)
+                par->RChild = temp;
+            else
+                par->LChild = temp;
+            // free(curr);
+        }
+        // node with double child deletion
         else
         {
-            throw k;
+            AVL_Node *prev = nullptr, *temp;
+            temp = curr->RChild;
+            stack.push(curr);
+            // inorder successor
+            while (temp->LChild != nullptr)
+            {
+                stack.push(temp);
+                prev = temp;
+                temp = temp->LChild;
+            }
+
+            if (prev != nullptr)
+                prev->LChild = temp->RChild;
+            else
+                curr->RChild = temp->RChild;
+            curr->key = temp->key;
+            k = temp->key;
+            // free(temp);
         }
     }
     catch (int x)
     {
-        cout << x << " is not presen the tree.";
+        cout << x << " is not present in the tree.";
+        return;
     }
-}
 
-AVL_Node *AVL_Tree::parentOfLeftMostNode(AVL_Node *curr)
-{
-    if (curr->LChild == nullptr)
+    AVL_Node *s, *t, *r, *p;
+    while (stack.top() != head)
     {
-        return nullptr;
+        s = stack.top();
+        int a = k < s->key ? 1 : -1;
+        stack.pop();
+        t = stack.top();
+
+        if (s->bf == a)
+        {
+            s->bf = 0;
+            continue;
+        }
+        else if (s->bf == 0)
+        {
+            s->bf = -1 * a;
+            return;
+        }
+        else
+        {
+            r = s->bf == 1 ? s->LChild : s->RChild;
+            //single rotation
+            if (r->bf == -1 * a)
+            {
+                if (a == -1)
+                {
+                    p = r;
+                    s->LChild = r->RChild;
+                    r->RChild = s;
+                    s->bf = 0;
+                    r->bf = 0;
+                }
+                else if (a == 1)
+                {
+                    p = r;
+                    s->RChild = r->LChild;
+                    r->LChild = s;
+                    s->bf = 0;
+                    r->bf = 0;
+                }
+            }
+            //single rotation
+            else if (r->bf == 0)
+            {
+                if (a == -1)
+                {
+                    p = r;
+                    s->LChild = r->RChild;
+                    r->RChild = s;
+                    r->bf = a;
+                }
+                else if (a == 1)
+                {
+                    p = r;
+                    s->RChild = r->LChild;
+                    r->LChild = s;
+                    r->bf = a;
+                }
+            }
+            //double rotation
+            else if (r->bf == a)
+            {
+                if (a == -1)
+                {
+                    p = r->RChild;
+                    r->RChild = p->LChild;
+                    p->LChild = r;
+                    s->LChild = p->RChild;
+                    p->RChild = s;
+                    s->bf = p->bf == 0 ? 0 : (p->bf == 1 ? -1 : 0);
+                    r->bf = p->bf == 0 ? 0 : (p->bf == 1 ? 0 : 1);
+                    p->bf = 0;
+                }
+                else if (a == 1)
+                {
+                    p = r->LChild;
+                    r->LChild = p->RChild;
+                    p->RChild = r;
+                    s->RChild = p->LChild;
+                    p->LChild = s;
+                    s->bf = p->bf == 0 ? 0 : (p->bf == 1 ? 0 : 1);
+                    r->bf = p->bf == 0 ? 0 : (p->bf == 1 ? -1 : 0);
+                    p->bf = 0;
+                }
+            }
+        }
+        if (s == t->RChild)
+            t->RChild = p;
+        else
+            t->LChild = p;
     }
-    while (curr->LChild->LChild != nullptr)
-        curr = curr->LChild;
-    return curr;
 }
 
 bool AVL_Tree::AVL_Search(int k)
@@ -301,12 +382,11 @@ bool AVL_Tree::AVL_Search(int k)
     return false;
 }
 
-void AVL_Tree::AVL_Print()
+void AVL_Tree::AVL_Print(const char *filename)
 {
     // forming initial part of "graph.gv file"
     ofstream stream;
     string str = "graph";
-    str.append(to_string(result++));
     str.append(".gv");
     stream.open(str);
     stream << "digraph g { \n\tnode[shape = record, height = .1];\n";
@@ -365,6 +445,9 @@ void AVL_Tree::AVL_Print()
     // writing nodes details and pointer details on graph.gv
     stream << "}";
     stream.close();
+
+    string cmd = "dot.exe -Tpng graph.gv -o " + string(filename) + ".png";
+    system((const char *)cmd.c_str());
 }
 
 int main()
@@ -383,23 +466,36 @@ int main()
     tree.AVL_Insert(25);
     //tree.AVL_Print();
     tree.AVL_Insert(40);
+    tree.AVL_Insert(35);
     //tree.AVL_Print();
-    tree.AVL_Insert(11);
+    // tree.AVL_Insert(11);
     //tree.AVL_Print();
     tree.AVL_Insert(17);
     //tree.AVL_Print();
-    tree.AVL_Insert(35);
-    //tree.AVL_Print();
-    tree.AVL_Print();
-    tree.AVL_Delete(3);
-    tree.AVL_Print();
-    tree.AVL_Delete(40);
-    tree.AVL_Print();
-    tree.AVL_Delete(20);
-    tree.AVL_Print();
+    // tree.AVL_Insert(35);
+    // tree.AVL_Print();
+    // tree.AVL_Delete(15);
+    // tree.AVL_Print();
+    // tree.AVL_Delete(30);
+    // tree.AVL_Print();
+    // tree.AVL_Delete(15);
+    // tree.AVL_Print();
+    // tree.AVL_Delete(20);
+    // tree.AVL_Print();
+    // tree.AVL_Delete(30);
+    // tree.AVL_Delete(20);
+    // tree.AVL_Print();
+    // tree.AVL_Delete(40);
+    // tree.AVL_Print();
+    // tree.AVL_Delete(20);
+    // tree.AVL_Print();
     // tree.AVL_Delete(28);
     // tree.AVL_Print();
     // tree.AVL_Delete(5);
     // tree.AVL_Print();
     // cout << tree.AVL_Search(100) << endl;
+    string filename;
+    cout << "Enter file name: ";
+    cin >> filename;
+    tree.AVL_Print(filename.c_str());
 }
